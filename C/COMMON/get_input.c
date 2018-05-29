3664ac0bf5a23b8d5cdfa38a5e0ac2d84c86c742
esarver <rabidhamper7@gmail.com>
2018-05-29 15:02:24 -0400

    added a folder for MyInputs since we are discouraged from using bots to get AoC inputs due to server limitations. Started saving some inputs.

--- C/COMMON/get_input.c
+++ C/COMMON/get_input.c
@@ -4,6 +4,37 @@
 
 #include <curl/curl.h>
 
+/***************************************************************************
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
+ *                             \___|\___/|_| \_\_____|
+ *
+ * Copyright (C) 1998 - 2015, Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution. The terms
+ * are also available at https://curl.haxx.se/docs/copyright.html.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the COPYING file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/ 
+/* <DESC>
+ * Shows how the write callback function can be used to download data into a
+ * chunk of memory instead of storing it in a file.
+ * </DESC>
+ */ 
+
+/*
+Example URL for input: http://adventofcode.com/2015/day/1/input
+*/
+
 struct MemoryStruct {
 	char * memory;
 	size_t size;
