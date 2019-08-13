9adc51854261377f6582e2ebc30a9d47a2353a39
esarver <rabidhamper7@gmail.com>
2019-08-12 23:14:17 -0400

    More work on 2015, Day01

--- src/Rust/src/lib/y2015/Day01.rs
+++ src/Rust/src/lib/y2015/Day01.rs
@@ -1,9 +1,41 @@
-pub fn day01(input: &str) -> u64 {
+pub fn day01(input: &str) -> i64 {
 
+    let mut santa: Santa;
+
+    for instruction in input.chars() {
+        match instruction {
+            '(' => santa.up(),
+            ')' => santa.down(),
+            _ => {},
+        }
+    }
+
+    santa.floor
 }
 
-enum Direction {
-    Up,
-    Down
+trait Movement {
+    fn up(&self);
+    fn down(&self);
+}
+
+struct Santa {
+    floor: i64,
+    steps: u64
+}
+
+impl Santa {
+    fn new(starting_floor: i64 = 0){
+
+    }
+}
+
+impl Movement for Santa {
+    fn up(&self) {
+        self.floor = self.floor + 1;
+    }
+
+    fn down(&self) {
+        self.floor = self.floor - 1;
+    }
 }
 
