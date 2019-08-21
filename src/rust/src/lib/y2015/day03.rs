0ed5d74035f825d61688cb9977c257937bd928e0
esarver <esarver@users.noreply.github.com>
2019-08-21 13:11:42 -0400

    completed day 3 of 2015

--- src/rust/src/lib/y2015/day03.rs
+++ src/rust/src/lib/y2015/day03.rs
@@ -21,13 +21,29 @@ fn santa_movement() {
     assert_eq!(santa.current_location, Coordinates(0,0));
 }
 
-pub fn day03(_input: &str) -> Day {
+#[test]
+fn visited_houses(){
+    let mut santa = Santa::new();
+    let input = "^>v<^"; // Should only be 4 total housed
+
+    for mv in input.chars() {
+        santa.move_direction(mv);
+    }
+
+    assert_eq!(santa.number_visited_houses(), 4);
+}
+
+pub fn day03(input: &str) -> Day {
     let mut day = Day::new(3);
 
     day.add_part({
         Part::new("Part A", {
             let mut ret = Vec::new();
-            ret.push(Answer::None);
+            let mut santa = Santa::new();
+            for movement in input.chars() {
+                santa.move_direction(movement);
+            }
+            ret.push(Answer::Unsigned(santa.number_visited_houses()));
 
             ret
         })
@@ -35,13 +51,31 @@ pub fn day03(_input: &str) -> Day {
     day.add_part({
         Part::new("Part B", {
             let mut ret = Vec::new();
-            ret.push(Answer::None);
+            let mut santa = Santa::new();
+            let mut robo_santa = Santa::new();
+
+            for (i, movement) in input.chars().enumerate() {
+                if (i % 2) == 0 {
+                    santa.move_direction(movement);
+                } else {
+                    robo_santa.move_direction(movement);
+                }
+            }
+
+            ret.push(Answer::Unsigned(get_unique_visited(&santa, &robo_santa)));
 
             ret
         })
     });
 
     day
+}
+
+fn get_unique_visited(santa_1: &Santa, santa_2: &Santa) -> u64 {
+    let mut merged = santa_1.visited_locations.clone();
+    merged.extend(santa_2.visited_locations.clone());
+
+    merged.keys().len() as u64
 }
 
 #[derive(PartialEq, Eq, Hash, Debug, Clone)]
@@ -76,5 +110,9 @@ impl Santa {
             Some(x) => {*x += 1;}
             _ => {self.visited_locations.insert(self.current_location.clone(), 1);},
         }
+    }
+
+    fn number_visited_houses(&self) -> u64 {
+        self.visited_locations.keys().len() as u64
     }
 }
