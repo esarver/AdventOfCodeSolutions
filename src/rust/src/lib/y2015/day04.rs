05c581f6dd38fa8546ea4eb7b4e22b4539b81f45
esarver <esarver@users.noreply.github.com>
2019-08-21 17:21:24 -0400

    Finished Day 04: 2015 Need to find a better way to run these... Part B is REALLY expensive...

--- src/rust/src/lib/y2015/day04.rs
+++ src/rust/src/lib/y2015/day04.rs
@@ -1,12 +1,36 @@
 use crate::lib::common::answer::Answer;
 use crate::lib::common::day::Day;
 use crate::lib::common::part::Part;
+extern crate md5;
+
+#[test]
+#[ignore]
+fn given_a() {
+    assert_eq!(part_a_answer("abcdef"), Answer::Unsigned(609_043));
+}
+
+#[test]
+#[ignore]
+fn given_b() {
+    assert_eq!(part_a_answer("pqrstuv"), Answer::Unsigned(1_048_970));
+}
+
+#[test]
+#[ignore]
+fn test_md5_a() {
+    let to_test = "abcdef609043".to_string();
+    let hash = calculate_hash(to_test.as_str());
+
+    println!("Hash: {}",hash);
+    println!("First 5: {}", hash.split_at(5).0);
+}
 
 #[test]
 fn part_a() {
     use crate::lib::common::input;
+    println!("Input: [{}]", input::get_input("2015", "4"));
     let part_a = part_a_answer(input::get_input("2015", "4").as_str());
-    assert_eq!(part_a, Answer::None);
+    assert_eq!(part_a, Answer::Unsigned(346_386));
 }
 
 #[test]
@@ -38,10 +62,28 @@ pub fn day04(input: &str) -> Day {
     day
 }
 
-fn part_a_answer(_input: &str) -> Answer {
-    Answer::None
+fn part_a_answer(input: &str) -> Answer {
+    Answer::Unsigned(find_lowest_positive_number(input.trim(), 5))
 }
 
-fn part_b_answer(_input: &str) -> Answer {
-    Answer::None
+fn part_b_answer(input: &str) -> Answer {
+    Answer::Unsigned(find_lowest_positive_number(input.trim(), 6))
+}
+
+fn find_lowest_positive_number(secret_key: &str, num_leading_zeroes: u8) -> u64 {
+    for x in 0..u32::max_value() {
+        let test_val = format!("{}{}", secret_key, x); 
+        let hash = calculate_hash(test_val.as_str());
+        let (interest, _) = hash.split_at(usize::from(num_leading_zeroes));
+        let t = "0".repeat(usize::from(num_leading_zeroes));
+        if t == interest {
+            return u64::from(x);
+        }
+
+    }
+    panic!("No value satisfies the rules!");
+}
+
+fn calculate_hash(to_hash: &str) -> String {
+    format!("{:X}",md5::compute(to_hash.as_bytes()))
 }
--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,8 +1,6 @@
 mod lib;
 use lib::y2015;
 fn main() {
-    let mut years = Vec::new();
-    years.push(y2015::y2015());
 
     let intro = r#"
 
@@ -15,6 +13,10 @@ _/  _\\__,_| \_/\___|_| _|\__| \___/_|   \___|\___/\__,_|\___|
     "#; // Generated using the "smshadow" font at https://www.kammerl.de/ascii/AsciiSignature.php
 
     println!("{}", intro);
+
+
+    let mut years = Vec::new();
+    years.push(y2015::y2015());
 
     for year in years {
         println!("{}", year);
