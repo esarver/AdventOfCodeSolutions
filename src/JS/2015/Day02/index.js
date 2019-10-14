c2bd84da1b37a8879519ad6400372abb4e06ca73
Edwin Sarver <rabidhamper7@gmail.com>
2019-10-14 10:31:39 -0400

    accept the rust future

--- src/JS/2015/Day02/index.js
+++
@@ -1,6 +0,0 @@
-var logic = require('./logic');
-var { input } = require ('./input');
-
-var partOne = logic.GetTotalSurfaceArea(input);
-
-console.log("PART ONE: " + partOne);
--- src/lib/common/input.rs
+++ src/lib/common/input.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 pub fn get_input(year: &str, day: &str) -> String {
-    let input_path = format!("../../inputs/{}/{}/input.txt", year, day);
+    let input_path = format!("inputs/{}/{}/input.txt", year, day);
 
     fs::read_to_string(&input_path)
         .unwrap_or_else(|_| panic!("File \"{}\" could not be retreived.", &input_path))
